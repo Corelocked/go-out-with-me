76dd8e5bbecc0032376ba4bd5e04c176074a0755
Corelocked <115864258+Corelocked@users.noreply.github.com>
2024-06-08 08:16:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,34 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const gif = document.getElementById('gif');
     const container = document.querySelector('.container');
     const h1 = document.getElementById('proposal-text');
+    let noAttempts = 0;
 
-    let noClicks = 0;
-
-    yesBtn.addEventListener('click', async (event) => {
-        event.preventDefault();
-        gif.src = 'pizza.gif';
-        h1.textContent = 'I love you';
-        yesBtn.remove();
-        noBtn.remove();
-
-        // Send the number of no clicks to the server
+    const sendNoAttempts = async (noAttempts) => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/send_email', {
+            const response = await fetch('http://127.0.0.1:5000/track_attempt', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({ no_clicks: noClicks }),
+                body: JSON.stringify({ no_attempts: noAttempts })
             });
             const result = await response.json();
             console.log(result.message);
         } catch (error) {
-            console.error('Error:', error);
+            console.error('Error sending data:', error);
         }
-    });
+    };
 
     noBtn.addEventListener('mouseover', (event) => {
-        noClicks += 1;
         const containerRect = container.getBoundingClientRect();
         const maxX = containerRect.width - noBtn.offsetWidth;
         const maxY = containerRect.height - noBtn.offsetHeight;
@@ -46,5 +37,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const currentSize = parseFloat(window.getComputedStyle(yesBtn).fontSize);
         yesBtn.style.fontSize = (currentSize + 5) + 'px';
+
+        noAttempts++;
+    });
+
+    yesBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        gif.src = 'pizza.gif';
+        h1.textContent = 'I love you';
+        yesBtn.remove();
+        noBtn.remove();
+        sendNoAttempts(noAttempts);
     });
 });
